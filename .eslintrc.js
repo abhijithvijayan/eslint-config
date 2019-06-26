abc924708e56a8e588a9f6f40ef45b10ac71877d
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-06-26 18:21:28 +0530

    react-hooks dependency field

--- .eslintrc.js
+++ .eslintrc.js
@@ -79,6 +79,7 @@ module.exports = {
     "react/no-unescaped-entities": 0,
     "react/destructuring-assignment": 0,
     "react/prop-types": 0,
+    "react-hooks/exhaustive-deps": 0,
     "jsx-a11y/accessible-emoji": 0,
     "react/require-default-props": 0,
     "react/jsx-filename-extension": [
