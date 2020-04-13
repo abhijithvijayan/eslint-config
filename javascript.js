e37f36e0f08daa8d51f9b28dd59641fb159e9cbb
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-04-13 14:35:01 +0530

    chore: set up js module

---
+++ javascript.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "extends": [
+    "./non-rules.js",
+    "airbnb", // Uses rules from `eslint-config-airbnb`
+    "prettier",
+    "./base.js"
+  ],
+  "parser": "babel-eslint",
+  "rules": {},
+}
