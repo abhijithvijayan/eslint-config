e5c5290d3b957af91377e1c7c5228bc349abd239
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-04-13 14:30:58 +0530

    feat: set up base config module

---
+++ base.js
@@ -0,0 +1,121 @@
+module.exports = {
+   "rules": {
+      "arrow-body-style": [
+         "error",
+         "as-needed",
+         {
+            "requireReturnForObjectLiteral":true
+         }
+      ],
+      "comma-dangle": 0,
+      "consistent-return": 0,
+      "func-names": 0,
+      "import": 0,
+      "import/extensions": 0,
+      "import/prefer-default-export": 0,
+      "indent": [
+         "error",
+         "tab",
+         {
+            "SwitchCase": 1,
+            "MemberExpression": 1,
+            "ArrayExpression": 1,
+            "ObjectExpression": 1,
+            "ImportDeclaration": 1,
+            "flatTernaryExpressions": false
+         }
+      ],
+      "jsx-a11y/accessible-emoji": 0,
+      "jsx-a11y/anchor-is-valid": [
+         "warn",
+         {
+            "aspects": [
+               "invalidHref"
+            ]
+         }
+      ],
+      "jsx-a11y/href-no-hash": "off",
+      "max-len": 0,
+      "no-alert": 0,
+      "no-await-in-loop": 0,
+      "no-console": 2,
+      "no-debugger": 0,
+      "no-param-reassign": [
+         2,
+         {
+            "props": false
+         }
+      ],
+      "no-restricted-syntax": [
+         2,
+         "ForInStatement",
+         "LabeledStatement",
+         "WithStatement"
+      ],
+      "no-return-assign": [
+         "error",
+         "except-parens"
+      ],
+      "no-shadow": [
+         2,
+         {
+            "allow": [
+               "resolve",
+               "reject",
+               "done",
+               "next",
+               "err",
+               "error"
+            ],
+            "hoist": "all"
+         }
+      ],
+      "no-underscore-dangle": 0,
+      "no-unused-expressions": [
+         2,
+         {
+            "allowTaggedTemplates": true
+         }
+      ],
+      "no-unused-vars": [
+         1,
+         {
+            "argsIgnorePattern": "res|next|^err",
+            "ignoreSiblings": true
+         }
+      ],
+      "prefer-const": [
+         "error",
+         {
+            "destructuring": "all"
+         }
+      ],
+      "prettier/prettier": [
+         "error",
+         {
+            "printWidth": 120,
+            "semi": true,
+            "singleQuote": true,
+            "tabWidth": 4,
+            "trailingComma": "es5"
+         }
+      ],
+      "quotes": [
+         2,
+         "single",
+         {
+            "allowTemplateLiterals": true,
+            "avoidEscape": true
+         }
+      ],
+      "radix": 0,
+      "space-before-function-paren": [
+         "error",
+         {
+            "anonymous": "never",
+            "named": "never",
+            "asyncArrow": "always"
+         }
+      ]
+   }
+}
