d95018d11b5be680b7599b0e1f8ce030b34e64cb
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-04-14 09:11:38 +0530

    fix: sort jsx-a11y rules

--- jsx-a11y.js
+++ jsx-a11y.js
@@ -23,6 +23,7 @@ module.exports = {
       "jsx-a11y/aria-role": "error",
       "jsx-a11y/aria-unsupported-elements": "error",
       "jsx-a11y/click-events-have-key-events": "error",
+      "jsx-a11y/control-has-associated-label": "off",
       "jsx-a11y/heading-has-content": "error",
       "jsx-a11y/href-no-hash": "off",
       "jsx-a11y/html-has-lang": "error",
@@ -30,6 +31,7 @@ module.exports = {
       "jsx-a11y/img-redundant-alt": "error",
       "jsx-a11y/interactive-supports-focus": "warn",
       "jsx-a11y/label-has-for": "off",
+      "jsx-a11y/label-has-associated-control": "error",
       "jsx-a11y/lang": "error",
       "jsx-a11y/media-has-caption": "warn",
       "jsx-a11y/mouse-events-have-key-events": "error",
@@ -45,8 +47,6 @@ module.exports = {
       "jsx-a11y/role-has-required-aria-props": "error",
       "jsx-a11y/role-supports-aria-props": "error",
       "jsx-a11y/scope": "error",
-      "jsx-a11y/tabindex-no-positive": "warn",
-      "jsx-a11y/label-has-associated-control": "error",
-      "jsx-a11y/control-has-associated-label": "off",
+      "jsx-a11y/tabindex-no-positive": "warn"
     }
 }
