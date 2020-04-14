796c83785cdf15d11c62129a6934c778a873fc9e
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-04-14 08:42:55 +0530

    fix: prettier plugin issue

--- prettier.js
+++ prettier.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "plugins": [
+  "extends": [
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   "rules": {
