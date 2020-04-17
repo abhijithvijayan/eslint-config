d1d6df77376aec1b67a97ec315860ff3d0366da2
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-04-17 11:53:57 +0530

    feat: add some awesome react rules

--- react.js
+++ react.js
@@ -17,9 +17,13 @@ module.exports = {
   ],
   // react rules documentation https://github.com/yannickcr/eslint-plugin-react#list-of-supported-rules
   "rules": {
+      // Forbid "button" element without an explicit "type" attribute
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/button-has-type.md
+      "react/button-has-type": "error",
+
       // Enforce consistent usage of destructuring assignment of props, state, and context
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
-      "react/destructuring-assignment": "off",
+      "react/destructuring-assignment": "always",
 
       // Prevent missing displayName in a React component definition
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/display-name.md
@@ -44,15 +48,51 @@ module.exports = {
 
       // Prevent usage of Array index in keys
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-array-index-key.md
-      "react/no-array-index-key": "off",
+      "react/no-array-index-key": "error",
+
+      // Prevent usage of dangerous JSX props
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-danger.md
+      "react/no-danger": "error",
+
+      // Report when a DOM element is using both children and dangerouslySetInnerHTML
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-danger-with-children.md
+      "react/no-danger-with-children": "error",
+
+      // Prevent usage of deprecated methods
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-deprecated.md
+      "react/no-deprecated": "error",
+
+      // Prevent direct mutation of this.state
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-direct-mutation-state.md
+      "react/no-direct-mutation-state": "error",
 
       // Prevent multiple component definition per file
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-multi-comp.md
       "react/no-multi-comp": "off",
 
+      // Prevent usage of the return value of React.render
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-render-return-value.md
+      "react/no-render-return-value": "error",
+
+      // Report "this" being used in stateless components
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-this-in-sfc.md
+      "react/no-this-in-sfc": "error",
+
+      // Prevent common typos
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-typos.md
+      "react/no-typos": "error",
+
       // Detect unescaped HTML entities, which might represent malformed tags
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unescaped-entities.md
-      "react/no-unescaped-entities": "off",
+      "react/no-unescaped-entities": "error",
+
+      // Prevent usage of setState
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-set-state.md
+      "react/no-set-state": "error",
+
+      // Prevent usage of unsafe lifecycle methods
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unsafe.md
+      "react/no-unsafe": "error",
 
       // Enforce stateless components to be written as a pure function
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md
@@ -68,7 +108,25 @@ module.exports = {
 
       // Enforce a defaultProps definition for every prop that is not a required prop
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-default-props.md
-      "react/require-default-props": "off"
+      "react/require-default-props": "off",
+
+      // Enforce ES5 or ES6 class for returning value in render function
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-render-return.md
+      "react/require-render-return": "error",
+
+      // Prevent extra closing tags for components without children (fixable)
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
+      "react/self-closing-comp": [
+        "error",
+        {
+          "component": true,
+          "html": true
+        }
+      ],
+
+      // State initialization in an ES6 class component should be in a constructor
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/state-in-constructor.md
+      "react/state-in-constructor": "error"
   },
   "settings": {
       // Override `eslint-plugin-import` rule
