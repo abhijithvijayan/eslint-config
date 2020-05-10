48d29a09e359a23e1989a5ae09cf8c2c72f77aed
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-05-10 23:54:02 +0530

    chore: add some rules comments

--- base.js
+++ base.js
@@ -3,6 +3,8 @@ module.exports = {
       "airbnb" // Uses rules from `eslint-config-airbnb`
    ],
    "rules": {
+      // Require braces in arrow function body
+      // https://eslint.org/docs/rules/arrow-body-style
       "arrow-body-style": [
          "error",
          "as-needed",
@@ -10,30 +12,58 @@ module.exports = {
             "requireReturnForObjectLiteral": true
          }
       ],
+
+      // Require or disallow trailing commas
+      // https://eslint.org/docs/rules/comma-dangle
       "comma-dangle": "off",
-      "consistent-return": "off",
-      "func-names": "off",
+
+      // Require or disallow named function expressions
+      // https://eslint.org/docs/rules/func-names
+      "func-names": "error",
+
+      // Enforce a maximum line length
+      // https://eslint.org/docs/rules/max-len
       "max-len": "off",
-      "no-alert": "off",
+
+      // Disallow Use of Alert
+      // https://eslint.org/docs/rules/no-alert
+      "no-alert": "error",
+
+      // Disallow await inside of loops
+      // https://eslint.org/docs/rules/no-await-in-loop
       "no-await-in-loop": "off",
+
+      // disallow the use of console
+      // https://eslint.org/docs/rules/no-console
       "no-console": "error",
-      "no-debugger": "off",
+
+      // Disallow Reassignment of Function Parameters
+      // https://eslint.org/docs/rules/no-param-reassign
       "no-param-reassign": [
          "error",
          {
             "props": false
          }
       ],
+
+      // disallow specified syntax
+      // https://eslint.org/docs/rules/no-restricted-syntax
       "no-restricted-syntax": [
          "error",
          "ForInStatement",
          "LabeledStatement",
          "WithStatement"
       ],
+
+      // Disallow Assignment in return Statement
+      // https://eslint.org/docs/rules/no-return-assign
       "no-return-assign": [
          "error",
          "except-parens"
       ],
+
+      // disallow variable declarations from shadowing variables declared in the outer scope
+      // https://eslint.org/docs/rules/no-shadow
       "no-shadow": [
          "error",
          {
@@ -48,6 +78,7 @@ module.exports = {
             "hoist": "all"
          }
       ],
+
       "no-underscore-dangle": "off",
       "no-unused-expressions": [
          "error",
